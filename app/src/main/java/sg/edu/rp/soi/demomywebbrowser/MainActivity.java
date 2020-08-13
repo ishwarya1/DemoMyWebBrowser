16319d1d4726e0b6a7c2e71729ac7a334e7ec077
18016638 <18016638@myrp.edu.sg>
2020-08-13 10:33:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/soi/demomywebbrowser/MainActivity.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.soi.demomywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLoadURL=findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.grab.com/sg/";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
